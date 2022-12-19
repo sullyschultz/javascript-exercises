e274ceb6814bcdecc6bc28cb2bb75dc9bce82dae
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-19 13:43:51 +0100

    repeatString. Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
+let stringText = "hey";
+
+
+const repeatString = function(stringText, num) {
+    if (num == 3) {
+     return stringText.repeat(num);
+    }
+    else if (num == 10) {
+     return stringText.repeat(num);
+    }
+    else if (num == 1) {
+        return stringText.repeat(num);
+    }
+    else if (num == 0 || stringText == "") {
+        return "";
+    }
+    else if (num <= -1) {
+        return "ERROR";
+    }
+    else {
+        return stringText.repeat(num)
+    }
 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+

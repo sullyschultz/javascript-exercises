f5e75bbd374dd55178e42d0cb5a1e0555f9db4e9
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-19 22:20:46 +0100

    sumAll. Exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if ( a < 0 || b < 0) return "ERROR";
+    if (a > b) {
+        const temp = a;
+        a = b;
+        b = temp;
+    }
+        let finalSum = 0;
+        for (let i = a; i <= b; i++) {
+            finalSum += i;
+                }
+                return finalSum;
+        
+
 
 };
 

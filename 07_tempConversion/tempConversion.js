35a7eb1574ff2536b5d361a04a13e028ca884f6b
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-20 12:40:25 +0100

    tempConversion. Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f, c) {
+  c = ((f-32) * 5 / 9);
+  const resultC = Math.round(c * 10) / 10;
+  return resultC;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c, f) {
+  f = (c * 9) / 5 + 32;
+  const resultF = Math.round(f * 10) / 10;
+  return resultF;
+
+
+
+  
 };
 
 // Do not edit below this line

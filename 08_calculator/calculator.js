407ed2f3519cd7ed9b656eeb78539887d6c396d6
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-29 13:43:16 +0100

    calculator exercise in javascript

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,49 @@
-const add = function() {
+const add = function(numOne, numTwo) {
+  const result = (numOne + numTwo);
+  return result;
 	
 };
 
-const subtract = function() {
+const subtract = function(numOne, numTwo) {
+  let result =(numOne - numTwo);
+  return result;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  
+  const initialValue = 0;
+  const totalValue = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue);
+    return totalValue;
+  };
+
+
+const multiply = function(array) {
+
+  let initialValue = 1;
+  const totalValue = array.reduce(
+    (accumulator, currentValue) => accumulator * currentValue,
+    initialValue);
+    return totalValue;
+};
+
+const power = function(numOne, numTwo) {
+  let total = Math.pow(numOne, numTwo);
+  return total;
 	
 };
 
-const multiply = function() {
-
+const factorial = function(num) {
+ if (num === 0) {
+  return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+ 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {

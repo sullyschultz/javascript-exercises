9186a592e5f5af6e0b04f46f049c2fd8240e85e1
sullyschultz <118315484+sullyschultz@users.noreply.github.com>
2022-12-29 14:15:12 +0100

    palindromes function exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let regExp = /[^a-z]/g;
+    let lowRegString = str.toLowerCase().replace(regExp, "");
+    let reverseString = lowRegString.split("").reverse().join("");
 
+    return reverseString == lowRegString;
+    
 };
 
 // Do not edit below this line
